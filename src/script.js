4b552fb3c3740ed2fa4dbc9e859a281128a934e7
Sasha <zzsanchozz@gmail.com>
2020-08-11 05:14:37 +0300

    first update

--- src/script.js
+++ src/script.js
@@ -1,9 +1,21 @@
-let clearButton = document.getElementById('clearLS');
 let data = document.getElementById('text');
 let b = document.getElementById('button')
-
+let arr = [];
+//add value to localStorage
 function submit() {
-    localStorage.setItem('text', data.value)
+    arr.push(data.value)
+    arr.push('1')
+    localStorage.setItem(arr.length() + 1, arr)
 }
 
-lS.addEventListener('submit', submit)
+form.addEventListener('submit', submit)
+
+//clear localStorage
+function clearLS() {
+    localStorage.clear();
+}
+
+clearL.addEventListener('click', clearLS)
+
+
+//problem to save previous result of localstorage to array
