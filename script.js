f0665138726e63a9ab2da23b4dcb84b66bf41f4c
Sasha <zzsanchozz@gmail.com>
2020-08-11 05:10:13 +0300

    replace all into src

--- script.js
+++
@@ -1,7 +0,0 @@
-let input = document.getElementById('data');
-
-
-function test(ar) {
-    localStorage.setItem('text', `${input.value}`)
-}
-input.oninput = test
---
+++ src/script.js
@@ -0,0 +1,9 @@
+let clearButton = document.getElementById('clearLS');
+let data = document.getElementById('text');
+let b = document.getElementById('button')
+
+function submit() {
+    localStorage.setItem('text', data.value)
+}
+
+lS.addEventListener('submit', submit)
