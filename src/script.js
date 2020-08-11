a8b0791c246623c9ac8032f2a034c64b4828a3af
Sasha <zzsanchozz@gmail.com>
2020-08-11 15:20:55 +0300

    add clear localStorage, list for localStorage

--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 let data = document.getElementById('text');
-let b = document.getElementById('button')
+
 let arr = [];
+
+let ulList = document.getElementById('list');
+
+
 //add value to localStorage
 function submit() {
     arr.push(data.value)
-    arr.push('1')
-    localStorage.setItem(arr.length() + 1, arr)
+    localStorage.setItem('data', arr)
+    return false;
 }
 
 form.addEventListener('submit', submit)
@@ -17,5 +21,14 @@ function clearLS() {
 
 clearL.addEventListener('click', clearLS)
 
+//add data to ul
 
-//problem to save previous result of localstorage to array
+function ul() {
+    let list = document.createElement('li');
+    // create list item, add data, append to ulList
+}
+
+
+form.addEventListener('submit', ul)
+    //problem to save previous result of localstorage to array
+    // see if ul function can be called when submit function is called
