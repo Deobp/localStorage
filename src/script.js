f32a8edab65a2ddae397b1fa82a4924fea3aefbb
Sasha <zzsanchozz@gmail.com>
2020-08-14 19:56:02 +0300

    fix comments

--- src/script.js
+++ src/script.js
@@ -1,50 +1,34 @@
-let data = document.getElementById('text');
+let data = document.getElementById("text");
 
-let ulList = document.getElementById('list');
+let ulList = document.getElementById("list");
 
-let dataList = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-
+let dataList = localStorage.getItem("items") ?
+    JSON.parse(localStorage.getItem("items")) :
+    [];
 
 //add value to localStorage
 function submit(event) {
-    dataList.push(data.value)
+    dataList.push(data.value);
 
-    localStorage.setItem('items', JSON.stringify(dataList));
-    ul(data.value)
-    data.value = '';
-    event.preventDefault()
+    localStorage.setItem("items", JSON.stringify(dataList));
+    ul(data.value);
+    data.value = "";
+    event.preventDefault();
 }
-dataList.forEach((item) => ul(item))
-form.addEventListener('submit', submit)
+dataList.forEach((item) => ul(item));
+form.addEventListener("submit", submit);
 
 //clear localStorage
 function clearLS() {
     localStorage.clear();
 }
 
-clearL.addEventListener('click', clearLS)
+clearL.addEventListener("click", clearLS);
 
-//add data to ul
 
-// function ul() {
-//     let json = JSON.stringify(localStorage);
-
-//     for (let key in json) {
-//         let item = document.createElement('li');
-//         item.value = 'yo'
-//         ulList.appendChild(item)
-//     }
-//}
-
+//add localStorage data to ul
 function ul(itemInList) {
-    let item = document.createElement('li');
-    item.textContent = itemInList
-    ulList.appendChild(item)
+    let item = document.createElement("li");
+    item.textContent = itemInList;
+    ulList.appendChild(item);
 }
-
-
-// create list item, add data, append to ulList
-
-
-//problem to save previous result of localstorage to array
-// see if ul function can be called when submit function is called
