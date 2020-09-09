c5a787aa1877297023af0c4936a796255f2d165c
LaievskyiSasha <47028393+LaievskyiSasha@users.noreply.github.com>
2020-09-09 17:22:43 +0300

    Update script.js
    changed name of submit function

--- src/script.js
+++ src/script.js
@@ -2,8 +2,8 @@ let clearButton = document.getElementById('clearLS');
 let data = document.getElementById('text');
 let lsButton = document.getElementById('button');
 
-function submit() {
+function dataToLs() {
     localStorage.setItem('text', data.value)
 }
 
-form.addEventListener('submit', submit);
+form.addEventListener('dataToLs', submit);
